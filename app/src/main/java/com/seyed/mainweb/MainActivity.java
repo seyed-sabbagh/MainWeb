000cd6ef7889403b914aa05a7961d5847086687e
seyed <mohammadjavadsabagh@gmail.com>
2021-06-07 15:09:23 +0430

    clean project

--- app/src/main/java/com/seyed/mainweb/MainActivity.java
+++ app/src/main/java/com/seyed/mainweb/MainActivity.java
@@ -6,27 +6,15 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.denzcoskun.imageslider.ImageSlider;
-import com.denzcoskun.imageslider.models.SlideModel;
-import java.util.ArrayList;
-import java.util.List;
-
 public class MainActivity extends AppCompatActivity {
     TextView txtapple, txtchanghal, txtjazebe, txteghamat, txtboomgardi, txttabiatghardi, txtsafarname, txtsoghat;
     Typeface typeface, typeface2;
-    ImageSlider slider_main;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        List<SlideModel> slideModels = new ArrayList<>();
-        slideModels.add(new SlideModel("",""));
-        slideModels.add(new SlideModel("",""));
-        slideModels.add(new SlideModel("",""));
-        slideModels.add(new SlideModel("",""));
-        slider_main.setImageList(slideModels,true);
 
         FindViewByID();
         Typface();
@@ -47,15 +35,14 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void FindViewByID() {
-        slider_main = findViewById(R.id.slider_main);
-        txtapple = (TextView) findViewById(R.id.txtapple);
-        txtchanghal = (TextView) findViewById(R.id.txtchanghal);
-        txtjazebe = (TextView) findViewById(R.id.txtjazebe);
-        txteghamat = (TextView) findViewById(R.id.txteghamat);
-        txtboomgardi = (TextView) findViewById(R.id.txtboomgardi);
-        txttabiatghardi = (TextView) findViewById(R.id.txttabiatghardi);
-        txtsafarname = (TextView) findViewById(R.id.txtsafarname);
-        txtsoghat = (TextView) findViewById(R.id.txtsoghat);
+        txtapple = findViewById(R.id.txtapple);
+        txtchanghal = findViewById(R.id.txtchanghal);
+        txtjazebe = findViewById(R.id.txtjazebe);
+        txteghamat = findViewById(R.id.txteghamat);
+        txtboomgardi = findViewById(R.id.txtboomgardi);
+        txttabiatghardi = findViewById(R.id.txttabiatghardi);
+        txtsafarname = findViewById(R.id.txtsafarname);
+        txtsoghat = findViewById(R.id.txtsoghat);
 
     }
 }
